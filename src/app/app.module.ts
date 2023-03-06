f5c4a3ff31411f3a2dcdb585a0dc4ae8177bc717
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-03-05 23:19:29 -0800

    --add header and styles, mock data in assets/data, fetching mock data to home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  trendingMovies: any;
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.getTrendingMovies();
+  }
+
+  getTrendingMovies() {
+this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe((movies) => {
+  console.log(movies);
+  this.trendingMovies = movies;
+  console.log('====================================');
+  console.log(movies);
+  console.log('====================================');
+  });
+}
 }
